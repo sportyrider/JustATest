faa48c3abfab91d0c5455d857bb3fc88ac46a351
sportyrider <martin.kurowski@gmail.com>
2017-01-11 21:50:11 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link } from 'react-router';
+
+import App from './components/App';
+
+ReactDOM.render(
+  (
+  <App />
+),
+  document.getElementById('container')
+);
